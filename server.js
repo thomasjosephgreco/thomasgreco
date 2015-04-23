9cf33cab918f1478f9691e9125b6590a5297fda7
Thomas Greco <Thomasjosephgreco@yahoo.com>
2015-04-23 02:14:12 -0400

    Created favicon for thomasgreco.me which I then used the serve-favicon module in correlation with express to correctly serve the file. Also, I added new meta tags as I begin to work on my SEO

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // set up ======================================================================
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
+var path 	 = require('path');
 var mongoose = require('mongoose'); 					// mongoose for mongodb
 var port  	 = process.env.PORT || 8080; 				// set the port
 var database = require('./config/database'); 			// load the database config
 var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var staticCache = require('express-static-cache');
+var favicon = require('serve-favicon');
 var ghost = require('ghost');
 
 
@@ -23,7 +26,7 @@ app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
+app.use(favicon(__dirname + '/public/thomasgrecodotme.ico'));
 
 // routes ======================================================================
 require('./app/routes.js')(app);
